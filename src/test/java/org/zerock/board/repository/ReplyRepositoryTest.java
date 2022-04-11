bd25686160d2888d07a50e45b78d671fbd556dd9
kimjisoo <kimjisoo3653@nate.com>
2022-04-12 00:41:40 +0900

    jpa N : 1

---
+++ src/test/java/org/zerock/board/repository/ReplyRepositoryTest.java
@@ -0,0 +1,49 @@
+package org.zerock.board.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Reply;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ReplyRepositoryTest {
+
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    void insertReply() {
+
+        IntStream.rangeClosed(1, 300).forEach(i -> {
+
+            long bno = (long)(Math.random() * 100) + 1;
+
+            Board board = Board.builder().bno(bno).build();
+
+            Reply reply = Reply.builder()
+                    .text("Reply......." + i)
+                    .board(board)
+                    .replyer("guest")
+                    .build();
+
+            replyRepository.save(reply);
+        });
+    }
+
+    @Test
+    void 조회() {
+
+        Optional<Reply> result = replyRepository.findById(1L);
+        Reply reply = result.get();
+
+        System.out.println("reply = " + reply);
+        System.out.println("reply.getBoard() = " + reply.getBoard());
+    }
+
+}
